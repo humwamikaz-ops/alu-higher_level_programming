1876483e4631f2eb0341b6067445c1a8a2a3b2f0
humwamikaz-ops <h.umwamikaz@alustudent.com>
2026-06-22 17:23:51 -0700

    pep8 val

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"  # noqa: E501
 str = str[39:67] + str[107:113] + str[:6]
 print(str)
