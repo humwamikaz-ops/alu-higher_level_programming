9c026fdb016a1e6776bd9177eae85867795f6f0f
Umwamikazi Honorata <h.umwamikaz@alustudent.com>
2026-08-01 06:29:43 +0000

    trial

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# sends a GET request with a custom header and displays the body
+# Sends a GET request with a custom header and displays the response body
+
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
