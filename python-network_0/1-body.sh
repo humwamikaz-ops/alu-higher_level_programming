1032b51549f10bbb488a67d3291fe6067f7198a9
Umwamikazi Honorata <h.umwamikaz@alustudent.com>
2026-08-01 06:41:49 +0000

    remodified

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request and displays the body only if status code is 200
-curl -s -o /tmp/response_body -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/response_body
+# Displays the body of the response only for a 200 status code
+curl -sL -o /tmp/body -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body
