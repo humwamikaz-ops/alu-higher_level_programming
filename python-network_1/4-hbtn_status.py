2936de221585d0bc08c13ce341662f7d96ece985
humwamikaz-ops <h.umwamikaz@alustudent.com>
2026-08-01 11:22:14 +0200

    Fix URL and formatting for Task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://alu-intranet.hbtn.io/status using requests"""
+"""
+Python script that fetches status from intranet using requests.
+"""
 import requests
 
+
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    url = "https://alx-intranet.hbtn.io/status"
+    r = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
